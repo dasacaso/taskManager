a5521917dc45e793579fe539558dd47e7c048eac
unknown <dasacaso@gmail.com>
2025-09-14 22:52:24 -0500

    Login y Register interfaces

--- frontend/task-manager/src/pages/Auth/Login.jsx
+++ frontend/task-manager/src/pages/Auth/Login.jsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import AuthLayout from "../../components/layouts/AuthLayout";
 import { Link, useNavigate } from "react-router-dom";
 import Input from "../../components/Inputs/Input";
+import { validateEmail } from "../../utils/helper";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -13,6 +16,41 @@ const Login = () => {
   // Handle Login Form Submit
   const handleLogin = async (e) => {
     e.preventDefault();
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+    if (!password) {
+      setError("Please enter your password.");
+      return;
+    }
+    setError("");
+
+    // Login API call
+    try {
+      const responce = await axiosInstance.post(API_PATHS.AUTH.LOGIN, {
+        email,
+        password,
+      });
+
+      const { token, role } = responce.data;
+
+      if (token) {
+        localStorage.setItem("token", token);
+        // Redirect based on role
+        if (role === "admin") {
+          navigate("/admin/dashboard");
+        } else {
+          navigate("/user/dashboard");
+        }      
+      }
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError("An error occurred. Please try again.");
+      }
+    }
   };
 
   return (
@@ -37,12 +75,10 @@ const Login = () => {
         />
 
         {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
-        <button
-          type="submit"
-          className="btn-primary">
+        <button type="submit" className="btn-primary">
             LOGIN
-          </button>
-          <p className="text-[13px] text-slate-800 mt-3">
+        </button>
+        <p className="text-[13px] text-slate-800 mt-3">
             Don't have an account?{" "}
             <Link
               className="font-medium text-primary underline" to="/signup">      
